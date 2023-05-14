1bccd9d2a20276dd1ccf474e35564b61dbe927a8
unknown <admin>
2023-05-14 09:10:35 +0700

    add new project test

---
+++ .vscode/BaiThucHanh1/PhanSo.java
@@ -0,0 +1,55 @@
+package BaiThucHanh1;
+
+public class PhanSo {
+    int ts;
+    int ms;
+    
+    public PhanSo() {
+        //khoi tao phan so 1
+    }
+    
+    public PhanSo(int ts, int ms) {
+        //khoi tao phan so 2
+    }
+
+    public PhanSo congPS(PhanSo ps)
+    {
+        int tsm = this.ts*?+this.ms*?;
+        int msm=?
+
+        return new PhanSo(tsm, msm);
+    }
+    public boolean ktToiGian(){
+
+    }
+
+    public PhanSo rutGonPS() {
+        int ucln = ?;
+        int tsm = ?;
+        int msm = ?;
+        return ?;
+    }
+
+    public int ucln (int a, int b)
+    {
+
+    }
+
+    public String inPS() {
+
+    }
+
+    public static class Test{
+        public static void public static void main(String[] args) {
+            
+            //tao phan so dung constructor khong tham so
+            PhanSo ps1 = ?
+            ps1.inPS();
+            PhanSo ps2 = ?
+            PhanSo ps3 =
+            PhanSo pst = ?
+            PhanSo psrg = ?
+        }
+    }
+
+}
